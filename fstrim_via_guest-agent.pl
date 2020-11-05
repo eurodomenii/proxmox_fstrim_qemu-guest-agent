c4630980f033bd9e354249f665dbba282ed0fc80
EuroDomenii <office@eurodomenii.com>
2020-11-05 02:11:13 +0200

    Linux classic mail function not working  Sh: 1: Syntax error: redirection unexpected  Dash does not have the <<< redirection operator via https://stackoverflow.com/questions/2462317/bash-syntax-error-redirection-unexpected/2462357#2462357. Let's switch to sendmail.

--- fstrim_via_guest-agent.pl
+++ fstrim_via_guest-agent.pl
@@ -9,15 +9,16 @@ my @vmids = `pvesh get /nodes/$node/qemu/ --output-format json-pretty | jq -r '.
 foreach my $i (@vmids) {
     chomp $i;
     my $fstrim_exec = "";
-    #$fstrim_exec = `qm guest exec $i fstrim -- "-av"`;
-    #print "Fstrim results for $i VM \n $fstrim_exec \n";
+    my $mail = "";
+    system '/usr/bin/mail -s "subject to root" root <<< "message"';
+
     $fstrim_exec = `qm guest exec $i fstrim -- "-av" | awk -F":" '/exited/{print \$2}'`;
     if ($fstrim_exec eq "") {
 	#This error code means QEMU guest agent is not running
 	#Test this by running in VM systemctl stop qemu-guest-agent.service
-	print "No agent for $i\n";
+	$mail = `mail -s "No agent for VM $i on node $node" root <<< "QEMU guest agent is not running for VM $i\n"`;
     } elsif (index($fstrim_exec, "0") != -1) {
-	print "Even QEMU guest agent is runnning, there's a execution error for $i VM";
+	$mail =`mail -s "Fstrim agent execution error on for VM $i on node $node" root <<< "Even QEMU guest agent is runnning, there's a execution error for VM $i"`;
     }
 }
 
