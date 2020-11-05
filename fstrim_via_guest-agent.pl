ec1cc916e3cebee6c8185f8a520eb7923787c2a9
EuroDomenii <office@eurodomenii.com>
2020-11-05 22:52:30 +0200

    Setup mail workflow plus print parameter mode

--- fstrim_via_guest-agent.pl
+++ fstrim_via_guest-agent.pl
@@ -2,23 +2,54 @@
 use strict;
 use warnings;
 
+my $mode = "mail";
+if (@ARGV && ($ARGV[0] eq "print")) {
+    $mode = "print";
+}
+
 my $node = `hostname`;
 chomp $node;
 my @vmids = `pvesh get /nodes/$node/qemu/ --output-format json-pretty | jq -r '.[] | .vmid'`;
 
+my $fstrim_exec = "";
+my $message = "";
+
 foreach my $i (@vmids) {
     chomp $i;
-    my $fstrim_exec = "";
-    my $mail = "";
-    system '/usr/bin/mail -s "subject to root" root <<< "message"';
-
-    $fstrim_exec = `qm guest exec $i fstrim -- "-av" | awk -F":" '/exited/{print \$2}'`;
-    if ($fstrim_exec eq "") {
-	#This error code means QEMU guest agent is not running
-	#Test this by running in VM systemctl stop qemu-guest-agent.service
-	$mail = `mail -s "No agent for VM $i on node $node" root <<< "QEMU guest agent is not running for VM $i\n"`;
-    } elsif (index($fstrim_exec, "0") != -1) {
-	$mail =`mail -s "Fstrim agent execution error on for VM $i on node $node" root <<< "Even QEMU guest agent is runnning, there's a execution error for VM $i"`;
+    if ($mode eq "print") {
+	$fstrim_exec = `qm guest exec $i fstrim -- "-av"`;
+	print "Fstrim results for $i VM \n $fstrim_exec \n";
+    } else {	
+        $fstrim_exec = `qm guest exec $i fstrim -- "-av" | awk -F":" '/exited/{print \$2}'`;
+        if ($fstrim_exec eq "") {
+	   #This error code means QEMU guest agent is not running
+	    #Test this by running in VM systemctl stop qemu-guest-agent.service
+	    $message .= "QEMU guest agent is not running for VM $i on node $node\n";
+	} elsif (index($fstrim_exec, "0") != -1) {
+	    $message .= "Even QEMU guest agent is runnning, there's a execution error of fstrim command for VM $i on node $node\n";
+        }
     }
 }
 
+if ($mode eq "mail") {
+    my $to = "root";
+    my $fullhostname = `hostname -f`;
+    $fullhostname =~ s/^\s+|\s+$//g ;
+    my $from = "root@".$fullhostname;
+    my $subject = "Fstrim discard report on Proxmox node $node";
+
+    open(MAIL, "|/usr/sbin/sendmail -t");
+
+    # Email Header
+    print MAIL "To: $to\n";
+    print MAIL "From: $from\n";
+    print MAIL "Subject: $subject\n\n";
+    # Email Body
+    print MAIL $message;
+
+    my $result = close(MAIL);
+    if(!$result){
+	#ToDo write to logs
+	#print "There was a problem, Bro!\n";
+    }
+}
